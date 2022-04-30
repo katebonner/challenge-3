bd90e087bcdb707db37c6a85f0b00a5e5a5f46ce
Kate Bonner <katebonner277@gmail.com>
2022-04-30 01:44:49 -0400

    updated javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,29 @@
-// Assignment code here
-
-
-// Get references to the #generate element
+// GET REFRENCES TO THE #generate ELEMENT
 var generateBtn = document.querySelector("#generate");
+
+//GLOBAL 
+var password = null;
 
 // Write password to the #password input
 function writePassword() {
 
-  // PASSWORD CRITERA OBJECT
+// ASSIGN PASSWORD CRITERA
   var passwordCritera = {
-    passLength: null,
+    passLength: 7,
     lowerCase: false,
     upperCase: false,
     numericalCharacters: false,
     specialCharacters: false
   }
 
-      // LENGTH: 7 < x < 129 CHARACTERS
-      passwordCritera.passLength = window.prompt("How long would you like your password to be? (please enter a number within the inclusive range of 8 to 128)");
-      console.log(passwordCritera.passLength);
-      
+while (passwordCritera.passLength < 8) {
+    // LENGTH: 7 < x < 129 CHARACTERS
+    passwordCritera.passLength = window.prompt("How long would you like your password to be? (please enter a number within the inclusive range of 8 to 128)");
+
+    if (passwordCritera.passLength >= 8)
+    break;
+  }
+    
   //LOOP THROUGH CHARACTER REQUIRMENTS UNTIL AT LEAST ONE IS SELECTED
   while (!(passwordCritera.lowerCase) && !(passwordCritera.upperCase) && !(passwordCritera.numericalCharacters) && !(passwordCritera.specialCharacters)) {
 
@@ -28,37 +32,88 @@ function writePassword() {
 
     // INCLUDE: LOWERCASE CHARACTERS
     passwordCritera.lowerCase = window.confirm("Would you like to include lowercase characters?");
-    console.log(passwordCritera.lowerCase);
 
     // INCLUDE: UPPERCASE CHARACTERS
     passwordCritera.upperCase = window.confirm("Would you like to include uppercase characters?");
-    console.log(passwordCritera.upperCase);
 
     // INCLUDE: NUMERICAL CHARACTERS
     passwordCritera.numericalCharacters = window.confirm("Would you like to include numerical characters?");
-    console.log(passwordCritera.numericalCharacters);
 
     // INCLUDE: SPECIAL CHARACTERS 
     passwordCritera.specialCharacters = window.confirm("Would you like to include special characters?");
-    console.log(passwordCritera.specialCharacters);
 
     if (!(passwordCritera.lowerCase) && !(passwordCritera.upperCase) && !(passwordCritera.numericalCharacters) && !(passwordCritera.specialCharacters)) {
       window.alert("No character requirements have been confirmed.")
     }
-
   }
+
+  // CHARACTER ARRAYS
+
+  var lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+  var upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+  var numbersArr = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+
+  var specialArr = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", ",", ".", "?"];
+    
+  var passwordArr = [];
+ 
+  var z = 0;
+  var zMax = passwordCritera.passLength;
+
+    while (z < zMax) {
+
+      if (passwordCritera.lowerCase) {
+        var i = Math.floor(Math.random()* (lowerCaseArr.length));
+        passwordArr.push(lowerCaseArr[i]);
+        console.log(z);
+        console.log(zMax);
+        z += 1;
+        if (z > zMax) {
+          break;
+        }
+      }
   
-  // GENERATE PASSWORD
-  var generatePassword = function() {
-    //INSERT LOGIC
-    generatedPassword = [];
-    return generatedPassword;
-  }
-  // ASSIGN GENERATED PASSWORD TO PASSWORD TEXT OBJECT
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+      if (passwordCritera.upperCase) {
+        var j = Math.floor(Math.random()* (upperCaseArr.length));
+        passwordArr.push(upperCaseArr[j]);
+        console.log(z);
+        console.log(zMax);
+        z += 1;
+        if (z > zMax) {
+          break;
+        }
+      }
+  
+      if (passwordCritera.numericalCharacters) {
+        var k = Math.floor(Math.random()* (numbersArr.length));
+        passwordArr.push(numbersArr[k]);
+        console.log(z);
+        console.log(zMax);
+        z += 1;
+        if (z > zMax) {
+          break;
+        }
+  
+      }
+  
+      if (passwordCritera.specialCharacters) {
+        var m = Math.floor(Math.random()* (specialArr.length));
+        passwordArr.push(specialArr[m]);
+        console.log(z);
+        console.log(zMax);
+        z += 1;
+        if (z > zMax) {
+          break;
+        }
+      }
+    }
 
+    // ASSIGN GENERATED PASSWORD ARRAY TO PASSWORD STRING
+    var password = passwordArr.join("");
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
 }
 
 // Add event listener to generate button
